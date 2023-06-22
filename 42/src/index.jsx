bfb311f9c187663394d967ee617a53bdf47587d7
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2023-06-22 14:36:06 +0100

    prettier config

--- 42/src/index.jsx
+++ 42/src/index.jsx
@@ -1,8 +1,6 @@
-import './style.css'
-import { createRoot } from 'react-dom/client'
+import "./style.css";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"));
 
-root.render(
-    <h1>Hello React</h1>
-)
+root.render(<h1 className="header-style">Hello React</h1>);
