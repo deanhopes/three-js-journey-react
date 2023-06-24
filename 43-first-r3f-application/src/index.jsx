e74e8e9b4eac558feaa7c62fff9ba98b0baa9b01
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2023-06-24 17:47:27 +0100

    added torus knot and increased the size of root

--- 43-first-r3f-application/src/index.jsx
+++ 43-first-r3f-application/src/index.jsx
@@ -1,10 +1,17 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <>
-        Soon to be a badass R3F application
-    </>
-)
+  <>
+    <Canvas>
+      <mesh>
+        <ambientLight />
+        <torusKnotGeometry />
+        <meshBasicMaterial color="hotpink" />
+      </mesh>
+    </Canvas>
+  </>
+);
