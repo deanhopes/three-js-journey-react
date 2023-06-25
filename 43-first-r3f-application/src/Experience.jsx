c6f385d1e14e6726a87c7f31de893ece5817448a
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2023-06-25 10:00:42 +0100

    z

--- 43-first-r3f-application/src/Experience.jsx
+++ 43-first-r3f-application/src/Experience.jsx
@@ -1,38 +1,42 @@
-import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 
 export default function Experience() {
-  const cubeRef = useRef();
-  const groupRef = useRef();
+    const cubeRef = useRef()
+    const groupRef = useRef()
 
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta;
-    groupRef.current.rotation.y += delta;
-  });
+    useFrame((state, delta) => {
+        cubeRef.current.rotation.y += delta
+        groupRef.current.rotation.y += delta
+    })
 
-  return (
-    <>
-      <group ref={groupRef}>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshBasicMaterial color={"orange"} />
-        </mesh>
+    return (
+        <>
+            <group ref={groupRef}>
+                <mesh position-x={-2}>
+                    <sphereGeometry />
+                    <meshBasicMaterial color={"orange"} />
+                </mesh>
 
-        <mesh
-          ref={cubeRef}
-          rotation-y={Math.PI * 0.25}
-          position-x={2}
-          scale={1.5}
-        >
-          <boxGeometry scale={[1.5]} />
-          <meshBasicMaterial color={"mediumpurple"} />
-        </mesh>
-      </group>
+                <mesh
+                    ref={cubeRef}
+                    rotation-y={Math.PI * 0.25}
+                    position-x={2}
+                    scale={1.5}
+                >
+                    <boxGeometry scale={[1.5]} />
+                    <meshBasicMaterial color={"mediumpurple"} />
+                </mesh>
+            </group>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-        <planeGeometry />
-        <meshBasicMaterial color={"greenyellow"} />
-      </mesh>
-    </>
-  );
+            <mesh
+                position-y={-1}
+                rotation-x={-Math.PI * 0.5}
+                scale={10}
+            >
+                <planeGeometry />
+                <meshBasicMaterial color={"greenyellow"} />
+            </mesh>
+        </>
+    )
 }
--- 43-first-r3f-application/src/index.jsx
+++ 43-first-r3f-application/src/index.jsx
@@ -1,12 +1,12 @@
-import "./style.css";
-import ReactDOM from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
-import Experience from "./Experience.jsx";
+import "./style.css"
+import ReactDOM from "react-dom/client"
+import { Canvas } from "@react-three/fiber"
+import Experience from "./Experience.jsx"
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.querySelector("#root"))
 
 root.render(
-  <Canvas>
-    <Experience />
-  </Canvas>
-);
+    <Canvas>
+        <Experience />
+    </Canvas>
+)
