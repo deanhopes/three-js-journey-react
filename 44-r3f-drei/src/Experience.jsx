20abed2a4eae43bfb33e074b9ca277e04676b632
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2023-07-06 13:20:08 +0100

    sphere and text working, no refraction

--- 44-r3f-drei/src/Experience.jsx
+++ 44-r3f-drei/src/Experience.jsx
@@ -1,29 +1,20 @@
 import {
     OrbitControls,
     TransformControls,
-    PivotControls,
     Html,
-    Text,
-    Float,
-    MeshReflectorMaterial,
-    MeshTransmissionMaterial,
+    CubeCamera,
+    MeshRefractionMaterial,
 } from "@react-three/drei"
 import {useRef} from "react"
 import {useControls} from "leva"
 
 export default function Experience() {
-    const materialProps = useControls({
-        thickness: {value: 5, min: 0, max: 20},
-        roughness: {value: 0, min: 0, max: 1, step: 0.1},
-        clearcoat: {value: 1, min: 0, max: 1, step: 0.1},
-        clearcoatRoughness: {value: 0, min: 0, max: 1, step: 0.1},
-        transmission: {value: 1, min: 0.9, max: 1, step: 0.01},
-        ior: {value: 1.25, min: 1, max: 2.3, step: 0.05},
-        envMapIntensity: {value: 25, min: 0, max: 100, step: 1},
+    const {ior, color} = useControls({
+        ior: {value: -1.5, min: -3, max: 3, step: 0.1},
         color: "#ffffff",
-        attenuationTint: "#ffe79e",
-        attenuationDistance: {value: 0, min: 0, max: 1},
     })
+
+    const mesh = useRef()
 
     return (
         <>
@@ -40,20 +31,29 @@ export default function Experience() {
             <ambientLight intensity={0.5} />
 
             <Html
-                position={[0, 0, 0]}
+                position={[0, 0, -2]}
                 transform='none'
                 wrapperClass='hero-text_wrap'
             >
-                <h1 className='hero-text'>Test</h1>
+                <h1 className='hero-text'>This is a placeholder statement</h1>
             </Html>
 
-            <mesh
-                rotation-y={2}
-                position={[0, 0, 2]}
-            >
-                <sphereGeometry />
-                <meshPhysicalMaterial {...materialProps} />
-            </mesh>
+            <CubeCamera>
+                {(texture) => (
+                    <mesh
+                        ref={mesh}
+                        position={[0, 0, 0]}
+                    >
+                        <sphereGeometry args={[1, 32, 32]} />
+                        <MeshRefractionMaterial
+                            envMap={texture}
+                            ior={ior}
+                            color={color}
+                        />
+                        <meshStandardMaterial color='white' />
+                    </mesh>
+                )}
+            </CubeCamera>
         </>
     )
 }
